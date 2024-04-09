34d77957f773dae2c43ff28a1b651237de7b3ebb
= <cadutsei@gmail.com>
2024-04-09 19:36:51 +0300

    fix: fix linting(last time I hope)

--- src/page/post/ui/post-page.tsx
+++ src/page/post/ui/post-page.tsx
@@ -1,4 +1,5 @@
 import { useLoaderData } from 'react-router-dom';
+import { CommentSection } from '@/widgets/comment-section/ui/comment-section';
 import { Post } from '@/widgets/post';
 import { Loader } from '@/shared/ui/loader';
 
@@ -8,9 +9,15 @@ export function PostPage() {
   return (
     <>
       <Loader />
-      <div className="xl:w-1/2 xl:mx-auto">
-        <div className="">
+      <div className="xl:w-2/3 xl:mx-auto">
+        <div className="pt-5 md:pt-20">
           <Post postId={postId} />
+          <div className="px-3 sm:px-5 md:pl-5 text-gray-200 md:w-3/4 py-5">
+            <div className="mb-5 text-2xl text-gray-200">
+              Comments
+            </div>
+            <CommentSection postId={postId} />
+          </div>
         </div>
       </div>
     </>
--- src/page/root/ui/root.tsx
+++ src/page/root/ui/root.tsx
@@ -5,7 +5,7 @@ export function Root() {
   return (
     <>
       <Loader />
-      <div className="w-full h-[100vh] bg-gradient-to-r from-[rgba(4,2,42,1)] via-[rgba(4,2,55,1)] to-[rgba(4,34,48,1)] ">
+      <div className="w-full min-h-[100vh] bg-gradient-to-r from-[rgba(4,2,42,1)] via-[rgba(4,2,55,1)] to-[rgba(4,34,48,1)] ">
         <Outlet />
       </div>
     </>
